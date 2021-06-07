6fca40a0c803c604d669d83a4be17a1f79e98177
Jin Young Choi <jinyoungchoi@alumni.emory.edu>
2021-06-06 18:08:50 -0700

    wrote necessary boilerplate for components to be used

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+const Header = () => {
+    return (
+        <>
+            <header>
+                <h1>Modern-React</h1>
+
+                <p>React ecosystem is constantly evolving.</p>
+                <br />
+                <p>Here is a list of resources to be referred to in the future:</p>
+                <br />
+            </header>
+        </>
+    )
+}
+
+export default Header
